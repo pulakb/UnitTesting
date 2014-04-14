198ceea62d7731e8698796c2fac3e4ea5f0dc937
Pulak <bhattacharyyapulak@gmail.com>
2014-04-14 22:47:31 +0530

    Unit Test Cases for Controllers

--- AngularJS/app/js/controllers.js
+++ AngularJS/app/js/controllers.js
@@ -4,8 +4,66 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', [function() {
-
+  	/* MyCtrl1 */
   }])
   .controller('MyCtrl2', [function() {
+  	/* MyCtrl2 */
+  }])
+  .controller('AddSubtractCtrl', ['$scope', function($scope) {
+  	/* AddSubtractCtrl */
+  	$scope.counter = 0;
 
-  }]);
+  	$scope.add = function (amount) {
+  		$scope.counter += amount;
+  	};
+
+  	$scope.subtract = function (amount) {
+  		$scope.counter -= amount;
+  	};
+
+  }])
+  .controller('EditCtrl', ['$scope', 'project', function($scope, project) {
+  	/* EditCtrl */
+    $scope.project = project;
+
+    $scope.removeTeamMember = function(teamMember) {
+      var idx = $scope.project.teamMembers.indexOf(teamMember);
+      if(idx >= 0) {
+        $scope.project.teamMembers.splice(idx, 1);
+      }
+    };
+
+  }]).
+  controller('LoginCtrl1', ['$scope', function ($scope) {
+  	/* LoginCtrl1 */
+  	$scope.shouldShowLogin = true;
+
+  	$scope.showLogin = function () {
+		$scope.shouldShowLogin = !$scope.shouldShowLogin;
+  	};
+  }]).
+  controller('CarouselDemoCtrl', ['$scope', function ($scope) {
+  	/* CarouselDemoCtrl */
+  	$scope.myInterval = 5000;
+
+    var slides = $scope.slides = [];
+    var images = [
+        '/assets/img/package_tours/1.gif',
+        '/assets/img/package_tours/2.jpg',
+        '/assets/img/package_tours/3.jpg',
+        '/assets/img/package_tours/4.JPG',
+    ];
+
+    var k = 0;
+
+    $scope.addSlide = function() {
+        var newWidth = 200 + ((slides.length + (25 * slides.length)) % 150);
+        slides.push({
+            image: images[k],
+            text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+                ['Goa', 'Andaman', 'Himachal', 'Kerala'][slides.length % 4]
+        });
+
+        k++;
+    };
+   }]);
--- AngularJS/test/unit/controllersSpec.js
+++ AngularJS/test/unit/controllersSpec.js
@@ -2,19 +2,135 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function(){
+describe('controllers', function() {
+  var $scope,
+      ctrl,
+      $controller;
+
+  //Loading the module
   beforeEach(module('myApp.controllers'));
 
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('MyCtrl1');
-    expect(myCtrl1).toBeDefined();
+  //Inject rootScope and create a new scope for testing
+  beforeEach(inject(function ($rootScope, _$controller_) {
+    $scope = $rootScope.$new();
+    $controller = _$controller_;
   }));
 
-  it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('MyCtrl2');
-    expect(myCtrl2).toBeDefined();
-  }));
+
+
+  describe('Testing MyCtrl1 and MyCtrl2', function() {
+    it('should MyCtrl1 be defined', inject(function($controller) {
+      //spec body
+      var myCtrl1 = $controller('MyCtrl1');
+      expect(myCtrl1).toBeDefined();
+    }));
+
+    it('should MyCtrl2 be defined', inject(function($controller) {
+      //spec body
+      var myCtrl2 = $controller('MyCtrl2');
+      expect(myCtrl2).toBeDefined();
+    }));
+  });
+
+  describe('Testing AddSubtractCtrl', function() {
+    it('should increase counter in add method of AddSubtractCtrl', function () {
+      //Create an instance of 'AddSubtractCtrl' controller
+      ctrl = $controller('AddSubtractCtrl', {
+        $scope: $scope
+      });
+
+      //Execute 'add' method
+      $scope.add(10);
+
+      //Verify result
+      expect($scope.counter).toEqual(10);
+    });   
+
+    it('should substract counter in subtract method of AddSubtractCtrl', function () {
+      //Create an instance of 'AddSubtractCtrl' controller
+      ctrl = $controller('AddSubtractCtrl', {
+        $scope: $scope
+      });
+
+      //Execute 'subtract' method
+      $scope.subtract(100);
+
+      //Verify result
+      expect($scope.counter).toEqual(-100);
+    });
+  });
+
+  describe('Testing EditCtrl', function() {
+    it('should remove an existing team member in EditCtrl', function () {
+      var teamMember = {
+          name: "John"
+      };
+
+      //Creating an instance of 'EditCtrl' controller
+      var ctrl = $controller('EditCtrl', {
+        $scope: $scope,
+        project: {
+          teamMembers: [teamMember]
+        }
+      });
+
+      //verify the initial setup
+      expect($scope.project.teamMembers).toEqual([teamMember]);
+
+      //execute and verify results
+      $scope.removeTeamMember(teamMember);
+      expect($scope.project.teamMembers).toEqual([]);
+    });
+  });
+
+  describe('Testing AddSubtractCtrl', function() {
+    it('should set \'shouldShowLogin\' false if it is true in LoginCtrl1', function () {
+      //Create an instance of 'AddSubtractCtrl' controller
+      ctrl =  $controller('LoginCtrl1', {
+        $scope: $scope
+      });
+
+      //Initial Value is true
+      $scope.showLogin();
+      expect($scope.shouldShowLogin).toBeFalsy();
+
+      //Again call showLogin(), current value of shouldShowLogin is false 
+      $scope.showLogin();
+      expect($scope.shouldShowLogin).toBeTruthy();
+    });
+  });
+
+  describe('Testing CarouselDemoCtrl', function() {
+    it('should check how many times addSlide got called', function () {
+      //Create an instance of 'CarouselDemoCtrl' controller
+      ctrl =  $controller('CarouselDemoCtrl', {
+        $scope: $scope
+      });
+
+      spyOn($scope, 'addSlide');
+
+      for (var i=0; i<4; i++) {
+          $scope.addSlide();
+      }
+
+      expect($scope.addSlide.calls.length).toBe(4);
+    });
+
+    it('should check count of slides added using addSlide method', function () {
+      //Create an instance of 'CarouselDemoCtrl' controller
+      ctrl =  $controller('CarouselDemoCtrl', {
+        $scope: $scope
+      });
+
+      spyOn($scope, 'addSlide').andCallThrough();
+
+      for (var i=0; i<4; i++) {
+          $scope.addSlide();
+      }
+
+      expect($scope.slides.length).toBe(4);
+    });
+  });
+
 });
